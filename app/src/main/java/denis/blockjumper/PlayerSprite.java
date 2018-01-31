ad7a327d97325c1ba0b0b6f73883410a2f573c05
deniscou <deniscou@gmail.com>
2018-01-31 19:13:56 +0100

    Sprites cambiantes, mejorados bordes

--- app/src/main/java/denis/blockjumper/PlayerSprite.java
+++ app/src/main/java/denis/blockjumper/PlayerSprite.java
@@ -15,11 +15,13 @@ class PlayerSprite {
     private static final int MAX_SPEED_X_L = -20, MAX_SPEED_X_R = 20;
     private int x = 0, y = 0, xSpeed = 0, ySpeed = 0;
     private int MAP_WIDTH, MAP_HEIGHT, width, height;
-    private int currentFrame = 0, currentColumn = 0;
+    private int currentColumn = 1, currentRow = 0;
     private boolean canJump = false, jumped = false;
     private GameView gameView;
     private Bitmap bmp;
     private int moving = 0;
+    private int changeFrame = 0;
+    private int animationOrder = 0;
 
 
     public PlayerSprite(GameView gameView) {
@@ -33,16 +35,24 @@ class PlayerSprite {
         this.y = MAP_HEIGHT - this.height;
     }
 
+    private void changeFrame() {
+        if (++changeFrame >= 8) {
+            if (currentColumn >= 2) {
+                animationOrder = -1;
+            } else if (currentColumn <= 0) {
+                animationOrder = 1;
+            }
+            currentColumn += animationOrder;
+            changeFrame = 0;
+        }
+    }
+
     private void update() {
         int newX = x;
         int newY = y;
-        if (newX > MAP_WIDTH - width - xSpeed) {
-            xSpeed = -xSpeed / 2;
-        } else if (newX < 0) {
-            xSpeed = -xSpeed / 2;
-        }
+
         if (newY >= MAP_HEIGHT - height - ySpeed) {
-            y = MAP_HEIGHT - height;
+            newY = MAP_HEIGHT - height - ySpeed;
             if (jumped) {
                 ySpeed = -ySpeed / 2;
                 canJump = true;
@@ -59,34 +69,55 @@ class PlayerSprite {
                 ySpeed = gameView.GRAVITY;
             }
         }
+        // Comprobamos el bote en caso de haber
+        System.out.println((newX <= 0 + xSpeed) + " - " + newX + " - " + xSpeed);
+        if (newX >= MAP_WIDTH - width - xSpeed) {
+            xSpeed = -xSpeed / 2;
+        } else if (newX <= 0 - xSpeed) {
+            System.out.println("Esto se está cumpliendo");
+            xSpeed = -xSpeed / 2;
+        }
+        System.out.println(xSpeed);
+
         // Movemos horizontalmente
         if (moving == -1) {
+            currentRow = 1;
+            animationOrder = -1;
             if (xSpeed > MAX_SPEED_X_L) {
-                xSpeed-=2;
+                xSpeed -= 2;
             }
         } else if (moving == 1) {
+            currentRow = 2;
+            animationOrder = 1;
             if (xSpeed < MAX_SPEED_X_R) {
-                xSpeed+=2;
+                xSpeed += 2;
             }
         } else {
+            currentRow = 0;
+            animationOrder = 0;
             if (xSpeed > 0) {
-                xSpeed-=2;
+                xSpeed -= 2;
             } else if (xSpeed < 0) {
-                xSpeed+=2;
+                xSpeed += 2;
             }
         }
-        // Comprobamos que no se haya pasado del limite con ySpeed y xSpeed
-        if (y > MAP_HEIGHT - height - ySpeed) {
-            y = MAP_HEIGHT - height - ySpeed;
-        }
-        if (x-xSpeed <= 0) {
-            x = 0 + xSpeed;
-        } else if (x >= MAP_WIDTH - width - xSpeed) {
-            x = MAP_WIDTH - width - xSpeed;
-        }
-        newX = x + xSpeed;
-        newY = y + ySpeed;
 
+
+        // Comprobamos que sprite debe cargar
+        changeFrame();
+
+        newX = newX + xSpeed;
+        newY = newY + ySpeed;
+
+        // Comprobamos (por si acaso) que no se haya pasado del mapa
+        if (newX < 0) {
+            newX = 0;
+        } else if (newX > MAP_WIDTH - width) {
+            newX = MAP_WIDTH - width;
+        }
+        if (newY > MAP_HEIGHT - height) {
+            newY = MAP_HEIGHT - height;
+        }
         x = newX;
         y = newY;
     }
@@ -100,8 +131,8 @@ class PlayerSprite {
 
     public void draw(Canvas canvas) {
         update();
-        int srcX = currentFrame * width;
-        int srcY = currentColumn * height;
+        int srcX = currentColumn * width;
+        int srcY = currentRow * height;
         Rect src = new Rect(srcX, srcY, srcX + width, srcY + width);
         Rect dist = new Rect(x, y, x + width, y + height);
         canvas.drawBitmap(bmp, src, dist, null);
